40f1ad887fe5591e9534acbb8c7e88c9eee29bd8
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-04 16:38:52 +0200

    Single thread pool

---
+++ experiments/src/main/kotlin/otus/gbp/multithreading/singleThreadPool.kt
@@ -0,0 +1,13 @@
+package otus.gbp.multithreading
+
+import java.util.concurrent.Executors
+
+fun main() {
+    val executor = Executors.newSingleThreadExecutor()
+    repeat(10) {
+        executor.execute {
+            log { "Running a task..." }
+        }
+    }
+    executor.shutdown()
+}
