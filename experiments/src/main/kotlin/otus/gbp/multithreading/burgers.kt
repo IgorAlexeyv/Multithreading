87fd91e477e5d8845f1380c951fb5278c71c5669
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-05 10:45:04 +0200

    Sequential burger

---
+++ experiments/src/main/kotlin/otus/gbp/multithreading/burgers.kt
@@ -0,0 +1,64 @@
+package otus.gbp.multithreading
+
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+    log { "Делаем бургер..." }
+    val time = measureTimeMillis {
+        kneadDough()
+        grindMeat()
+        bakeBuns()
+        roastPatty()
+        assemble()
+    }
+    log { "Готово! Время: $time" }
+}
+
+suspend fun perform(takesTime: Int = 1000, block: () -> String) {
+    log { "Starting task: ${block()}" }
+    repeat(takesTime / 100) {
+        Thread.sleep(100)
+    }
+    log { "Finished task: ${block()}" }
+}
+
+suspend fun kneadDough() {
+    perform(1000) {
+        color(WHITE) {
+            "Месим тесто"
+        }
+    }
+}
+
+suspend fun grindMeat() {
+    perform(300) {
+        color(RED) {
+            "Рубим мясо"
+        }
+    }
+}
+
+suspend fun bakeBuns() {
+    perform( 2000) {
+        color(YELLOW) {
+            "Печем булку"
+        }
+    }
+}
+
+suspend fun roastPatty() {
+    perform( 1000) {
+        color(PURPLE) {
+            "Жарим котлету"
+        }
+    }
+}
+
+suspend fun assemble() {
+    perform(1000) {
+        color(GREEN) {
+            "Собираем"
+        }
+    }
+}
